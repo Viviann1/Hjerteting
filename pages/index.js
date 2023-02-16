b2eb141b16402887a9a3b6f2e5c37d5c1812cadb
Viviann1 <107039059+Viviann1@users.noreply.github.com>
2023-02-16 13:05:32 +0100

    bilde nr2 med padding

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import { Inter } from '@next/font/google';
 import Head from 'next/Head';
 import Image from 'next/image';
 import img1 from "../images/varmepute.jpg";
+import img2 from "../images/varmepute2.jpg";
 
 export default function Home() {
   return (
@@ -12,13 +13,14 @@ export default function Home() {
       <main>
         <div className='p-3'>
           <div className='max-w-full p-4 tracking-widest bg-red-300 border-2 border-red-900 h-1/4'>
-          <h1 className='tracking-widest text-8xl text-zinc-100'>h j e r t e t i n g</h1>
-          <p className='pt-4'>Her finner du håndlagede produkter hvor hvert enkelt produkt er unikt. Hvert produkt er nøye designet for å fungere best mulig.</p>
+          <h1 className='ml-6 tracking-widest text-8xl text-zinc-100'>h j e r t e t i n g</h1>
+          <p className='pt-4 mt-6 mb-6 ml-6'>Her finner du håndlagede produkter hvor hvert enkelt produkt er unikt. Hvert produkt er nøye designet for å fungere best mulig.</p>
           </div>
           <div className='flex flex-col pt-x 6'>
           <div className='mt-11'>
             <h2 className='text-4xl'>Varmeputer nakke</h2>
-            <Image src={img1} alt="picture of heatpillow" />
+            <Image src={img1} className='mt-7' alt="picture of heatpillow" />
+            <Image src={img2} className='mt-7' alt="details of heatpillow"  />
             
           </div>
           
