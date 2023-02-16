3808031da286b8e2eaab67c59d2f3852232c9f81
Viviann1 <107039059+Viviann1@users.noreply.github.com>
2023-02-16 11:31:53 +0100

    changed top

--- pages/index.js
+++ pages/index.js
@@ -9,93 +9,93 @@ export default function Home() {
       <title>h j e r t e t i n g</title>
       </Head>
       <main>
-        <div>
-          <div>
-          <h1 className='text-5xl'>hjerteting</h1>
-          <p>Her finner du håndlagede produkter hvor hvert enkelt produkt er unikt. Hvert produkt er nøye designet for å fungere best mulig.</p>
+        <div className='p-3'>
+          <div className='max-w-full p-4 tracking-widest bg-red-300 border-2 border-red-900 h-1/4'>
+          <h1 className='text-5xl tracking-widest'>hjerteting</h1>
+          <p className='pt-4'>Her finner du håndlagede produkter hvor hvert enkelt produkt er unikt. Hvert produkt er nøye designet for å fungere best mulig.</p>
           </div>
-          
-          <div>
+          <div className='flex flex-col pt-x 6'>
+          <div className='mt-11'>
             <h2>Varmeputer nakke</h2>
           </div>
           
-          <div>
+          <div className='mt-11'>
             <h2>Varmepute barn</h2>
           </div>
           
-          <div>
+          <div className='mt-11'>
           <h2>Ris og Lavendel</h2>
           </div>
 
-          <div>
+          <div className='mt-11'>
             <h2>Sovemaske</h2>
           </div>
 
-          <div>
+          <div className='mt-11'>
             <h2>Makeup pads</h2>
           </div>
 
-          <div>
+          <div className='mt-11'>
             <h2>Hårmote</h2>
           </div>
           
-          <div>
+          <div className='mt-11'>
           <h2>Kaniner</h2>
           </div>
           
-          <div>
+          <div className='mt-11'>
           <h2>Dukker</h2>
           </div>
 
-          <div>
+          <div className='mt-11'>
             <h2>Engler</h2>
           </div>
 
-          <div>
+          <div className='mt-11'>
             <h2>Vimpelrekker</h2>
           </div>
 
-          <div>
+          <div className='mt-11'>
             <h2>Hjerter</h2>
           </div>
           
-          <div>
+          <div className='mt-11'>
             <h2>Luer</h2>
           </div>
 
-          <div>
+          <div className='mt-11'>
             <h2>Pannebånd</h2>
           </div>
           
-          <div>
+          <div className='mt-11'>
             <h2>Hals</h2>
           </div>
 
-          <div>
+          <div className='mt-11'>
             <h2>Smekker</h2>
           </div>
           
-          <div>
+          <div className='mt-11'>
             <h2>Koseklut</h2>
           </div>
 
-          <div>
+          <div className='mt-11'>
             <h2>Varmepute små mager</h2>
           </div>
 
-          <div>
+          <div className='mt-11'>
             <h2>JUL</h2>
           </div>
           
-          <div>
+          <div className='mt-11'>
             <h2>PÅSKE</h2>
           </div>
 
-          <div>
+          <div className='mt-11'>
             <h2>HALLOWEEN</h2>
           </div>
           
-          
+          </div>
           
         </div>
       </main>
