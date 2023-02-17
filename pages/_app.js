4994529ea1945b474a6078706ddd8ba7cf1adf07
Viviann1 <107039059+Viviann1@users.noreply.github.com>
2023-02-17 11:53:30 +0100

    background image and more added

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,19 @@
 import Navbar from '@/components/navbar';
 import '../styles/globals.css';
+import { Montserrat } from '@next/font/google';
+
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  weight: '100',
+})
 
 export default function App({ Component, pageProps }) {
   return (
   <>
+  <main className={montserrat.className}>
   <Navbar /> 
   <Component {...pageProps} />
+  </main>
   </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
-import { Inter } from '@next/font/google';
 import Head from 'next/Head';
 import Image from 'next/image';
 
 import { Montserrat } from "@next/font/google";
+
+import backgroundimage from "../images/background.jpg";
 
 import img1 from "../images/varmepute.jpg";
 import img2 from "../images/varmepute2.jpg";
@@ -19,18 +20,18 @@ export default function Home() {
       <title>h j e r t e t i n g</title>
       </Head>
       <main>
+        <Image src={backgroundimage} width={400} className='w-screen opacity-50' /> 
         <div className='p-3'>
           <div className='max-w-full p-4 tracking-widest bg-red-300 border-2 border-red-900 h-1/4'>
           <h1 className='ml-6 tracking-widest text-8xl text-zinc-100'>h j e r t e t i n g</h1>
           <p className='pt-4 mt-6 mb-6 ml-6'>her finner du håndlagede produkter hvor hvert enkelt produkt er unikt. Hvert produkt er nøye designet for å fungere best mulig.</p>
           </div>
           
-          <section className='gap-9 columns-3'>
-          <div>
-            <h2 className='text-3xl mb-7 mt-7'>varmeputer nakke</h2>
-            <Image src={img1} className='pl-2' alt="picture of heatpillow" />
-            <Image src={img2} className='mt-7' alt="details of heatpillow"  />
-            
+          <section className=''>
+          <div className='flex gap-0'>
+            <h2 className='text-3xl mt-7'>varmeputer nakke</h2>
+            <Image src={img1} width={300} className='pl-2 mt-7' alt="picture of heatpillow" />
+            <Image src={img2} width={300} className='mt-7' alt="details of heatpillow"  />
           </div>
           
           <div className='mt-11'>
